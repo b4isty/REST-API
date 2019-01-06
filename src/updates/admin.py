b23c0f71242a8fb9237ad27cf2b9e11fc4fc6c96
b4isty <baishakhidasgupta24@gmail.com>
2019-01-06 22:05:39 +0530

    update models created

--- src/updates/admin.py
+++ src/updates/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Updates as UpdateModel
 
-# Register your models here.
+
+admin.site.register([UpdateModel])
--- src/updates/models.py
+++ src/updates/models.py
@@ -1,3 +1,23 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+
+
+def upload_update_image(instance, filename):
+    return "update/{user}/{filename}".format(user=instance.user, filename=filename)
+
+
+class Updates(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    content = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to=upload_update_image, blank=True, null=True)
+    updated = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content or ""
+
+
+
+
