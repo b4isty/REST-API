f0855fac8046fc7b18881c8cc2de840a913a3b9c
baishakhidapl <baishakhi@digitalaptech.com>
2019-01-29 14:24:00 +0530

    creaete, detail, update, delete api view

--- src/status/api/urls.py
+++ src/status/api/urls.py
@@ -1,12 +1,20 @@
 from django.conf.urls import url
-from .views import StatusListSearchAPIView, StatusAPIView
+from .views import (
+    StatusListSearchAPIView,
+    StatusAPIView,
+    StatusCreateAPIView,
+    StatusDetailAPIView,
+    StatusUpdateAPIView,
+    StatusDeleteAPIView,
+)
 
 urlpatterns = [
     url(r'^$', StatusAPIView.as_view()),
+    url(r'^create/$', StatusCreateAPIView.as_view()),
+    url(r'^(?P<pk>\d+)/$', StatusDetailAPIView.as_view()),
+    url(r'^(?P<pk>\d+)/update/$', StatusUpdateAPIView.as_view()),
+    url(r'^(?P<pk>\d+)/delete/$', StatusDeleteAPIView.as_view()),
+
     # url(r'^$', StatusListSearchAPIView.as_view()),
-    # url(r'^create/$', StatusCreateAPIView.as_view()),
-    # url(r'^(?P<id>.*)/$', StatusDetailAPIView.as_view()),
-    # url(r'^(?P<id>.*)/update/$', StatusUpdateAPIView.as_view()),
-    # url(r'^(?P<id>.*)/delete/$', StatusDeleteAPIView.as_view()),
 
 ]
--- src/status/api/views.py
+++ src/status/api/views.py
@@ -9,12 +9,12 @@ class StatusListSearchAPIView(APIView):
     permission_classes = []
     authentication_classes = []
 
-    def get(self,request, format=None):
+    def get(self, request, format=None):
         qs = Status.objects.all()
         serializer = StatusSerializer(qs, many=True)
         return Response(serializer.data)
 
-    def post(self, requests, format=None):
+    def post(self, request, format=None):
         qs = Status.objects.all()
         serializer = StatusSerializer(qs, many=True)
         return Response(serializer.data)
@@ -32,7 +32,44 @@ class StatusAPIView(generics.ListAPIView):
         if query is not None:
             qs = qs.filter(content__icontains=query)
         return qs
-#
+
+
+class StatusCreateAPIView(generics.CreateAPIView):
+    permission_classes = []
+    authentication_classes = []
+    queryset = Status.objects.all()
+    serializer_class = StatusSerializer
+
+    # def perform_create(self, serializer):
+    #     serializer.save(user=self.request.user)
+
+
+class StatusDetailAPIView(generics.RetrieveAPIView):
+    permission_classes = []
+    authentication_classes = []
+    queryset = Status.objects.all()
+    serializer_class = StatusSerializer
+    # lookup_field = 'id'  # 'slug
+
+    # def get_object(self, *args, **kwargs):
+    #     kwargs = self.kwargs
+    #     kw_id = kwargs.get('id')
+    #     return Status.objects.get(id=kw_id)
+
+
+class StatusUpdateAPIView(generics.UpdateAPIView):
+    permission_classes = []
+    authentication_classes = []
+    queryset = Status.objects.all()
+    serializer_class = StatusSerializer
+
+
+class StatusDeleteAPIView(generics.DestroyAPIView):
+    permission_classes = []
+    authentication_classes = []
+    queryset = Status.objects.all()
+    serializer_class = StatusSerializer
+
 # class StatusCreateAPIView(generics.CreateView):
 #     queryset = Status.objects.all()
 #     serializer_class = StatusForm
